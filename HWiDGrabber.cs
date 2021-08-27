2436c1e24ab368fb510c5c48420a979d890a0499
AllahClientDevs <87165768+AllahClientDevs@users.noreply.github.com>
2021-08-27 14:09:01 -0500

    Add files via upload

---
+++ HWiDGrabber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+public class HWiDGrabber
+{
+    public string DriveID(string ID = null)
+    {
+        return GetWin32("Win32_DiskDrive", ID ?? "SerialNumber");
+    }
+
+    public string GetWin32(string Selection, string ID)
+    {
+        using (ManagementObjectSearcher searcher = new ManagementObjectSearcher($"SELECT * FROM {Selection}"))
+        {
+            try
+            {
+                var collection = searcher.Get().GetEnumerator();
+                return collection.MoveNext() ? collection.Current[ID].ToString() : "0";
+            }
+            catch (ManagementException Error)
+            {
+                switch (Error.ErrorCode)
+                {
+                    case ManagementStatus.NotFound:
+                        return "ID Not Found";
+                    case ManagementStatus.InvalidClass:
+                        return "Invalid Class";
+                    default:
+                        return "0";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Allah_HWID_Grabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HWiDGrabber grabber = new HWiDGrabber();
+            Console.WriteLine(grabber.DriveID());
+            Console.ReadKey();
+        }
+    }
+}
